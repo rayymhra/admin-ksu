047c4cc898da5da55fb86e553702a2cfd222d3e0
rayymhra <rayraeilish@gmail.com>
2025-09-04 13:23:14 +0700

    model setup

---
+++ app/Models/CarouselImage.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CarouselImage extends Model
+{
+    protected $fillable = [
+        'platform',
+        'image',
+    ];
+}
---
+++ app/Models/FeaturedProduct.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class FeaturedProduct extends Model
+{
+    protected $fillable = [
+        'product_name',
+        'brand_name',
+        'image',
+    ];
+}
